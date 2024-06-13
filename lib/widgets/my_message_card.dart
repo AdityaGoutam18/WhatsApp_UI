155ab0887486dff5d4dd32d3968aea4dbc181daa
AdityaGoutam18 <aditya.dev10010@gmail.com>
2024-06-13 18:21:45 +0530

    first Commit

---
+++ lib/widgets/my_message_card.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class MyMessageCard extends StatelessWidget {
+  const MyMessageCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Align(
+      alignment: Alignment.centerRight,
+    );
+  }
+}
